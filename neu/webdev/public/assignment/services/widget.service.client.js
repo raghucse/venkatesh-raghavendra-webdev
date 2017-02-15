b1ca47af0a58c4a770ea0d2be6eb53c014c53fc7
Raghavendra venkatesh <raghucse@ccs.neu.edu>
2017-02-14 22:35:23 -0500

    json modification

--- neu/webdev/public/assignment/services/widget.service.client.js
+++ neu/webdev/public/assignment/services/widget.service.client.js
@@ -16,11 +16,11 @@
             { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
             { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
                 "url": "http://lorempixel.com/400/200/"},
-            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "Lorem ipsum"},
             { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
             { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
                 "url": "https://youtu.be/AM2Ivdi9c4E" },
-            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "Lorem ipsum"}
         ]
 
         var api = {
